697ede3fb57dfdae2e2f2b57332889e769eb4d10
MichaelTheTall <michaeljhnewman@gmail.com>
2018-08-27 10:05:14 +0100

    Initialised DB

---
+++ db/project_shop.sql
@@ -0,0 +1,21 @@
+DROP TABLE items;
+DROP TABLE manufacturers;
+
+CREATE TABLE manufacturers
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+  info TEXT
+);
+
+CREATE TABLE items
+(
+  id SERIAL8 primary key,
+  name VARCHAR(255) not null,
+  man_id INT8 REFERENCES manufacturers(id),
+  info TEXT,
+  stock INT8,
+  cost INT8,
+  sale INT8,
+  profit INT8
+);
