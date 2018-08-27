e8b957d174e1eadd69cdb25292730876b7697802
MichaelTheTall <michaeljhnewman@gmail.com>
2018-08-27 21:27:21 +0100

    added missing find method

--- models/item.rb
+++ models/item.rb
@@ -34,10 +34,39 @@ class Item
     @id = results.first()['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE items
+    SET
+    (
+      name,
+      man_id,
+      info,
+      stock,
+      cost,
+      sale
+    )
+    VALUES
+    (
+      $1, $2, $3, $4, $5, $6
+    )
+    WHERE id = $7"
+    values = [@name, @man_id, @info, @stock, @cost, @sale, @id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * FROM items"
     results = SqlRunner.run(sql)
     return results.map {|i| Item.new(i)}
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM items
+    WHERE id = $1"
+    values = [id]
+    i = SqlRunner.run(sql, values)
+    result = Item.new(i.first)
+    return result
   end
 
   def manufacturer()
