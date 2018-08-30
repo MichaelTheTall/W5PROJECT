9e85485787b9ac6b909a1f0d8e9df78ac4cfb26c
MichaelTheTall <michaeljhnewman@gmail.com>
2018-08-30 18:19:26 +0100

    heroku DB credentials

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,14 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'project_shop', host: 'localhost'})
+      # db = PG.connect({dbname: 'project_shop', host: 'localhost'})
+      db = PG.connect({
+        dbname: 'dbhrt4ki2270c',
+        host: 'ec2-54-235-86-226.compute-1.amazonaws.com',
+        port: 5432,
+        user: 'mjwwokhiwuilrk',
+        password: 'efe26a9e3a13012053d39ad4d1d82cb3feeab2e26bf1359a5a01853c8d217006'
+        })
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
